57d06f14781f17dc771e112312e53f5678ef8bed
CherifEmil <48601023+CherifEmil@users.noreply.github.com>
2022-02-22 17:53:02 -0500

    new version

--- javascript/windows.js
+++ javascript/windows.js
@@ -1,3 +1,43 @@
+/*-var-*/
+const a = []
+
+
+function Insert_Data(tab_id,arr_data) 
+{
+  let old_tbody = document.getElementById(tab_id);
+  var new_tbody = document.createElement('tbody');
+  new_tbody.setAttribute("id",tab_id);
+  for(let i=0;i<arr_data.length;i++)
+  {
+  var row = new_tbody.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  cell1.innerHTML = arr_data[i].from;
+  cell2.innerHTML = arr_data[i].message;
+  cell3.innerHTML = arr_data[i].time;
+  }
+  old_tbody.parentNode.replaceChild(new_tbody, old_tbody);
+}
+///---------------------------------------------------------------
+Create_Data_M = function (tableD){
+    let Mail = document.getElementById("address_mail");
+    let Content = document.getElementById("content_mail");
+    //----
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    //------
+    tableD.push({from:Mail.value,message:Content.value,time:dateTime});
+    }
+    ///------
+    let submit = document.querySelector(".Pdep .contenu2 .send_mail");
+    submit.onclick=function(){
+        Create_Data_M(a)
+    
+    }
+//-----------------------------------------------------------------
 
 let read = document.querySelector("#read");
 let send = document.querySelector("#send");
@@ -6,48 +46,30 @@ let titre = document.querySelector(".Pdep .titre1 .T_ecrire");
 let contenu1 = document.querySelector(".Pdep .contenu1");
 let contenu2 = document.querySelector(".Pdep .contenu2");
 let contenu3 = document.querySelector(".Pdep  .contenu3");
-
-function Insert_Data(tab_id,arr_data) 
-{
-  let old_tbody = document.getElementById(tab_id);
-  var new_tbody = document.createElement('tbody');
-  for(let i=0;i<arr_data.length;i++)
-  {
-  var row = new_tbody.insertRow(0);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  var cell3 = row.insertCell(2);
-  cell1.innerHTML = arr_data[i].from;
-  cell2.innerHTML = arr_data[i].subject;
-  cell3.innerHTML = arr_data[i].time;
-  }
-  old_tbody.parentNode.replaceChild(new_tbody, old_tbody)
-}
-var a = [{from:"s",subject:"2",message:"",time:"4"},{from:"s",subject:"2",message:"",time:"4"},{from:"s",subject:"2",message:"",time:"4"}]
-
-
-
+let iconecontact = document.querySelector(".Pdep  .add_rem_contact");
 read.onclick = function(){
-    titre.innerHTML="liste des message";
+    titre.innerHTML="liste des messages";
     contenu1.style.display = "block";
     contenu2.style.display = "none";
     contenu3.style.display = "none";
-    Insert_Data("M_body",a);
-
-    
+    iconecontact.style.display = "none";
 }
 send.onclick = function(){
     titre.innerHTML="Ecrire un message";
     contenu1.style.display = "none";
     contenu2.style.display = "block";
     contenu3.style.display = "none";
-    
+    iconecontact.style.display = "none";
 }
 contacts.onclick = function(){
-    titre.innerHTML="Liste des contacte";
+    titre.innerHTML="Liste des contactes";
     contenu1.style.display = "none";
     contenu2.style.display = "none";
     contenu3.style.display = "block";
-    
+    iconecontact.style.display = "block";
 }
+setInterval(() => { 
+    Insert_Data("C_body",a);
+    Insert_Data("M_body",a);
+}, 2000);
 
