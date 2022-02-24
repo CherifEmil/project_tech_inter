e5f82c622e9e5c127a52a29d939a13be960f0848
CherifEmil <48601023+CherifEmil@users.noreply.github.com>
2022-02-23 22:30:09 -0500

    update on read mail
    On peut mtn acceder a deux tableaux
    l'un c'est pour les message recu et l'autre pour les messages envoyé .

--- javascript/windows.js
+++ javascript/windows.js
@@ -1,6 +1,7 @@
 /*-var-*/
-const a = []
-
+const a = [];
+const b = [];
+//------------------------------insert data to table-------------------------------
 
 function Insert_Data(tab_id,arr_data) 
 {
@@ -19,7 +20,10 @@ function Insert_Data(tab_id,arr_data)
   }
   old_tbody.parentNode.replaceChild(new_tbody, old_tbody);
 }
-///---------------------------------------------------------------
+///--------------------------Remplisseur de tableau -------------------------------------
+
+
+///-------- recu ou envoyé//--
 Create_Data_M = function (tableD){
     let Mail = document.getElementById("address_mail");
     let Content = document.getElementById("content_mail");
@@ -35,21 +39,50 @@ Create_Data_M = function (tableD){
     let submit = document.querySelector(".Pdep .contenu2 .send_mail");
     submit.onclick=function(){
         Create_Data_M(a)
-    
     }
+
+///------------
+
+///------------
 //-----------------------------------------------------------------
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------------------------------------------
 let read = document.querySelector("#read");
 let send = document.querySelector("#send");
 let contacts = document.querySelector("#contacts");
 let titre = document.querySelector(".Pdep .titre1 .T_ecrire");
 let contenu1 = document.querySelector(".Pdep .contenu1");
+let send_receive=document.querySelector(".Pdep .content_tabel_mail");
+let receive_I=document.querySelector("#received_M");
+let send_I=document.querySelector("#send_M");
 let contenu2 = document.querySelector(".Pdep .contenu2");
 let contenu3 = document.querySelector(".Pdep  .contenu3");
 let iconecontact = document.querySelector(".Pdep  .add_rem_contact");
 read.onclick = function(){
     titre.innerHTML="liste des messages";
     contenu1.style.display = "block";
+    send_receive.style.display = "block";
     contenu2.style.display = "none";
     contenu3.style.display = "none";
     iconecontact.style.display = "none";
@@ -57,6 +90,7 @@ read.onclick = function(){
 send.onclick = function(){
     titre.innerHTML="Ecrire un message";
     contenu1.style.display = "none";
+    send_receive.style.display = "none";
     contenu2.style.display = "block";
     contenu3.style.display = "none";
     iconecontact.style.display = "none";
@@ -64,12 +98,26 @@ send.onclick = function(){
 contacts.onclick = function(){
     titre.innerHTML="Liste des contactes";
     contenu1.style.display = "none";
+    send_receive.style.display = "none";
     contenu2.style.display = "none";
     contenu3.style.display = "block";
     iconecontact.style.display = "block";
 }
+var tab_selector=a;
+let prec_Autor= document.querySelector("#prec_to");
+receive_I.onclick=function(){
+    prec_Autor.innerHTML="De";
+    tab_selector=a;
+    Insert_Data("M_body",tab_selector)
+}
+send_I.onclick=function(){
+    prec_Autor.innerHTML="À       ";
+    tab_selector=b;
+    Insert_Data("M_body",tab_selector);
+}
+
 setInterval(() => { 
     Insert_Data("C_body",a);
-    Insert_Data("M_body",a);
+    Insert_Data("M_body",tab_selector);
 }, 2000);
 
